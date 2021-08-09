9ca066e0072c7d02c8f2ca524cb2a62c37947a81
Jack Cullen <jpcullen20@gmail.com>
2021-08-09 12:36:37 -0600

    Add video download

--- app/src/components/MediaCard.js
+++ app/src/components/MediaCard.js
@@ -1,6 +1,15 @@
 import React from 'react'
-import { Card, CardActionArea, CardActions, CardMedia, Button, ImageListItem } from '@material-ui/core'
+import { 
+    Card,
+    CardActionArea,
+    CardActions,
+    CardMedia,
+    ImageListItem,
+    IconButton,
+} from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
+import LinkIcon from '@material-ui/icons/Link';
+import GetAppIcon from '@material-ui/icons/GetApp';
 
 const useStyles = makeStyles({
   root: {
@@ -15,9 +24,33 @@ const useStyles = makeStyles({
   },
 });
 
-
 const MediaCard = ({ media }) => {
     const classes = useStyles();
+
+    const downloadFile = (mediaUrl) => {
+            fetch(mediaUrl, {
+            method: 'GET',
+            mode: 'no-cors',
+            headers: {
+                'Content-Type': 'video/mp4',
+            },
+        })
+        .then((response) => response.blob())
+        .then((blob) => {
+            const url = window.URL.createObjectURL(
+            new Blob([blob]),
+            );
+            const link = document.createElement('a');
+            link.href = url;
+            link.setAttribute(
+            'download',
+            `${mediaUrl}.mp4`,
+            );
+            document.body.appendChild(link);
+            link.click();
+            link.parentNode.removeChild(link);
+        });
+    }
 
     return (
         <ImageListItem className={classes.root}>
@@ -30,12 +63,12 @@ const MediaCard = ({ media }) => {
                     />
                 </CardActionArea>
                 <CardActions>
-                    <Button size="small" color="primary">
-                        Share
-                    </Button>
-                    <Button size="small" color="primary">
-                        Learn More
-                    </Button>
+                    <IconButton aria-label="link" onClick={() => navigator.clipboard.writeText(media.tracking_link)}>
+                        <LinkIcon />
+                    </IconButton>
+                    <IconButton aria-label="download" onClick={() => downloadFile(media.download_url)}>
+                        <GetAppIcon />
+                    </IconButton>
                 </CardActions>
             </Card>
         </ImageListItem>
