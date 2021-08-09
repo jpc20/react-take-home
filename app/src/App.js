1aa6fd81f93cb2de10f397188c3799a129d37fdc
Jack Cullen <jpcullen20@gmail.com>
2021-08-09 10:41:57 -0600

    Add initial feed and campaign components

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography } from '@material-ui/core';
+import Feed from './components/Feed.js'
 import './App.css';
 
+const useStyles = makeStyles({
+  root: {
+    textAlign: "center",
+    justifyContent: "center",
+  },
+});
+
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <Typography>Plugs</Typography>
+      <Feed />
     </div>
   );
 }
---
+++ app/src/components/Feed.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Campaign from './Campaign.js'
+
+const Feed = () => {
+    return (
+        <div>
+            <Campaign />
+        </div>
+    )
+}
+
+export default Feed
