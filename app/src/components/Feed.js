f83afafb1072d5124de5970e3c58b38c56b622c4
Jack Cullen <jpcullen20@gmail.com>
2021-08-09 10:53:16 -0600

    Fetch campaigns and iterate over all 8

--- app/src/components/Feed.js
+++ app/src/components/Feed.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Campaign from './Campaign.js'
 
+
 const Feed = () => {
+    const [campaigns, setCampaigns] = useState([])
+    useEffect(() => {
+            fetch(
+          "https://www.plugco.in/public/take_home_sample_feed",
+          {
+            headers: { accept: "application/json" },
+          }
+        )
+          .then((resp) => resp.json())
+          .then((data) => setCampaigns(data.campaigns));
+    }, [])
+
     return (
         <div>
-            <Campaign />
+            {
+                campaigns.map((campaign) => (
+                    <div>
+                        campaign
+                    </div>
+                ))
+            }
         </div>
     )
 }
